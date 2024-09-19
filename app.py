c97c952c83297fc0635df97cb12cd46e0ef8702e
vaidas <valikonis.vaidas@gmail.com>
2024-09-19 13:24:39 +0300

    Updated CORS ip's

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from web.news_feed_controller import news_bp
 from web.weather_controller import weather_bp
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "http://localhost:5173"])
+CORS(app, origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:4173"])
 app.register_blueprint(weather_bp)
 app.register_blueprint(news_bp)
 
